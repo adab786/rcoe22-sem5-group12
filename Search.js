366a5779daff6865f0c42e9a38ea214e591c66c2
Khansalman780 <92108727+Khansalman7860@users.noreply.github.com>
2022-10-12 19:15:09 +0530

    header  and Search

---
+++ Search.js
@@ -0,0 +1,29 @@
+
+
+import { InputBase, Box, styled } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+const SearchContainer = styled(Box)`
+    background: #fff;
+    width: 38%;
+    border-radius: 2px;
+    margin-left: 10px;
+`
+const InputSearchBase = styled(InputBase)`
+    padding-left: 20;
+    width: 100%;
+`
+
+const Search = () => {
+    return (
+      <SearchContainer>
+            <InputSearchBase
+                placeholder="Search for products, brands and more"
+            />
+            <Box>
+                <SearchIcon />
+            </Box>
+      </SearchContainer>
+    );
+}
+
+export default Search;
